4595ff2013124aa9655cf9703e39c2c3500c87d3
Chris Roth <c.roth.lambda@gmail.com>
2019-08-23 00:01:59 -0500

    MVP day 2 done: added lecture notes, removed hardcoded program, implemented file open passing args, got Mult to print the 72 from the register

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,12 +7,8 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.load()
-        self.alu()
-        self.trace()
-        self.run()
         self.reg = [0] * 8
-        self.memory = [0] * 256
+        self.ram = [0] * 256
         self.pc = 0
 
     def load(self):
@@ -22,19 +18,28 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+       
+        try:
+            with open( sys.argv[1]) as file:
+                for line in file:
+                    comment_split = line.split('#')
+                    possible_number = comment_split[0]
+                    if possible_number == '':
+                        continue
+                    first_bit = possible_number[0]
+                    if first_bit == '1' or first_bit == '0':
+                        instruction = int(possible_number[:8], 2)
+                        self.ram[address] = instruction
+                        address +=1
+                        
+        except FileNotFoundError:
+            print(f'Unable to find {sys.argv[1]}, please check the name and try again')
+            sys.exit(2)
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -74,20 +79,32 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-       ir = self.pc
-       operand_a = self.ram_read(self.ps +1)
-       operand_b = self.ram_read(self.pc + 2)
-       running = True
+        ir = self.pc
+        running = True
        
         while running:
+            operand_a = self.ram_read(ir +1)
+            operand_b = self.ram_read(ir + 2)
+            # print(f'operand1: {operand_a}')
+            # print(f'operand2: {operand_b}')
+
             if self.ram_read(ir) == 0b10000010: #LDI
                 self.reg[operand_a] = int(operand_b)
+                print('ldi')
                 ir += 3
             elif self.ram_read(ir) == 0b01000111: #PRN
-                print("did work", self.reg[int(operand_a)])
+                print( 'print', self.reg[int(operand_a)])
                 ir += 2
+            elif self.ram_read(ir) == 0b10100010: #MULT
+                print('mult')
+                mult_sum = self.reg[operand_a] * self.reg[operand_b]
+                self.reg[operand_a] = mult_sum
+                ir += 3
             elif self.ram_read(ir) == 0b00000001: #HLT
+                print('halt')
                 running = False
 
-        
- 
+
+
+
+
